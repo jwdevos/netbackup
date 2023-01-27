e0e83c29d4e168488349d963fd89bb79f3944bd6
Jaap de Vos <github@jaapdevos.com>
2023-01-27 16:29:20 +0100

    Update run.sh

--- cfg/run.sh
+++ cfg/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-LOG_PATH="/home/user/customers/home/logs/"
-BCK_PATH="/home/user/customers/home/backups/"
-CSV_PATH="/home/user/customers/home/cfg/.csv"
-ENV_PATH="/home/user/customers/home/cfg/.env"
+LOG_PATH="/home/user/customers/customer-1/logs/"
+BCK_PATH="/home/user/customers/customer-1/backups/"
+CSV_PATH="/home/user/customers/customer-1/cfg/.csv"
+ENV_PATH="/home/user/customers/customer-1/cfg/.env"
+REP_PATH="/home/user/customers/customer-1/cfg/report.j2"
 
 source /home/user/python/netbackup/bin/activate
-python /home/user/python/netbackup/main.py -l $LOG_PATH -b $BCK_PATH -c $CSV_PATH -e $ENV_PATH
+python /home/user/python/netbackup/main.py -l $LOG_PATH -b $BCK_PATH -c $CSV_PATH -e $ENV_PATH -r $REP_PATH
